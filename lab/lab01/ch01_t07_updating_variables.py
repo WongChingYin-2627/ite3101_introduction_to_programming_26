19c120a660fdf49514041b06c6c5445965b82d43
WongChingYin-2627 <biwong118@gmail.com>
2026-09-24 09:15:47 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -1,7 +1,7 @@
 january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
 annual_rainfall = january_to_june_rainfall
 
-july_rainfall =1.05
+july_rainfall =05
 annual_rainfall += july_rainfall
 
 august_rainfall + 4.91
