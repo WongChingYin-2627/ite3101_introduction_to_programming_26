0da01928b073c9ea9b14bcd567f4a1d70a5c1986
WongChingYin-2627 <biwong118@gmail.com>
2026-09-24 08:52:38 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -1,2 +1,2 @@
 annual_rainfall += (50+45+30+20)
-annual_rainfall += annual_rainfall += september_rain += october_rain + november_rain + december_rain
+annual_rainfall += annual_rainfall += september_rain += october_rain += november_rain + december_rain
